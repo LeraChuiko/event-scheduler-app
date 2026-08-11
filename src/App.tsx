9302d6868dabab6f0c5e44c7d8984694687b59df
Lera Chuiko <lera.chuiko@gmail.com>
2026-08-11 09:34:26 +0200

    chore: initial setup with Vite, React, TS and Tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold text-blue-400 mb-2">
+        Event Scheduler App
+      </h1>
+      <p className="text-slate-300">React + TypeScript + Tailwind v4</p>
+    </div>
+  );
+};
+
+export default App;
